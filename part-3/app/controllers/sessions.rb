1cc588eedcac23b93fe43cdc1433f5782ae61fce
Melissa Kanhirun <mk7run@gmail.com>
2017-10-25 12:09:14 -0500

    Add user, session, and index controller

---
+++ part-3/app/controllers/sessions.rb
@@ -0,0 +1,24 @@
+get '/sessions/new' do
+  erb :"sessions/new"
+end
+
+post '/sessions' do
+  @user = User.find_by(email: params[:email])
+
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = ["Either your email or password was wrong"]
+    erb :"sessions/new"
+  end
+end
+
+delete '/sessions' do
+  session.delete(:user_id)
+  redirect '/'
+end
+
+get '/not_authorized' do
+  erb :not_authorized
+end
---
+++ part-3/app/controllers/users.rb
@@ -0,0 +1,22 @@
+get '/users/new' do
+  @user = User.new
+  erb :"users/new"
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors.full_messages
+    erb :"users/new"
+  end
+end
+
+get '/users/:id' do
+  authenticate!
+  @user = User.find_by(id: params[:id])
+  authorize!(@user)
+  erb :"users/show"
+end

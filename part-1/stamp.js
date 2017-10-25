d309501e8f0807e7b11cc26457006ff24a9f5436
Melissa Kanhirun <mk7run@gmail.com>
2017-10-25 09:20:33 -0500

    average stamp value

--- part-1/stamp.js
+++ part-1/stamp.js
@@ -2,3 +2,23 @@
 /*   It is required to use a for loop                           */
 /*   when iterating over a stamp's array of appraisal values.   */
 /****************************************************************/
+
+var Stamp = function(args){
+  this.name = args.name;
+  this.issueDate = args.issueDate;
+  this.appraisalValues = args.appraisalValues
+}
+
+Stamp.prototype.totalAppraisal = function(){
+  var stampValues = this.appraisalValues;
+  for (var i = 0; i < stampValues.length; i++){
+    return stampValues.reduce(function(sum, value){
+      return sum + value
+    });
+    return (total/ this.appraisalValues.length)
+  };
+};
+
+Stamp.prototype.averageAppraisal = function(){
+  return (this.totalAppraisal() / this.appraisalValues.length)
+}

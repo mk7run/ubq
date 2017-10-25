d70042b75b16002928459c0879122e37de036820
Melissa Kanhirun <mk7run@gmail.com>
2017-10-25 16:44:34 -0500

    Format user show page

--- part-3/app/helpers/potluck_event_handler.rb
+++
@@ -1,8 +0,0 @@
-module PotluckEventControllerHelper
-  def find_dish(event)
-    potluckrsvp = PotluckAttendance.find_by(potluck_id: event.id)
-    potluckrsvp.dish
-  end
-end
-
-helpers PotluckEventControllerHelper

2787c561176c1e67d97799a1f8ba414a3730a62d
Melissa Kanhirun <mk7run@gmail.com>
2017-10-25 09:42:27 -0500

    stamp collection

--- part-1/stamp_collection.js
+++ part-1/stamp_collection.js
@@ -2,3 +2,27 @@
 /*   It is required to use Array.prototype methods                */
 /*   when iterating over a stamp collections's array of stamps.   */
 /******************************************************************/
+
+var StampCollection = function(stamps){
+  this.stamps = stamps
+};
+
+StampCollection.prototype.stampNamed = function(stampName){
+  return this.stamps.find(function(stamp){
+    return stamp.name === stampName;
+  });
+};
+
+StampCollection.prototype.stampsIssued = function(stampYear){
+  return this.stamps.filter(function(stamp){
+    return stamp.issueDate === stampYear;
+  })
+};
+
+StampCollection.prototype.value = function(){
+  var maxStampValues = 0
+  this.stamps.forEach(function(stamp){
+    maxStampValues += stamp.maximumAppraisal()
+  });
+  return maxStampValues
+};

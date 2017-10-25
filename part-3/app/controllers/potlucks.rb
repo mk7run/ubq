a30edb51800a830a22f7d5df17bd5febd43b5f0a
Melissa Kanhirun <mk7run@gmail.com>
2017-10-25 12:59:43 -0500

    Display all current potlucks and individual potluck show page

---
+++ part-3/app/controllers/potlucks.rb
@@ -0,0 +1,60 @@
+get '/potlucks' do
+  @upcoming_potlucks_events = Potluck.order(:starts_at)
+  @current_potlucks = @upcoming_potlucks_events.current
+
+  @potlucks = @upcoming_potlucks_events
+  erb :"/potlucks/index"
+end
+
+# get '/potlucks/new' do
+#   authenticate!
+#   erb :"/potlucks/new"
+# end
+
+# post '/potlucks' do
+#   @potluck = potluck.new(params[:potluck])
+#   authenticate!
+
+#   if @potluck.valid?
+#     current_user.potlucks << @potlucks
+#     redirect "/potlucks/#{@potluck.id}"
+#   else
+#     @errors = @potlucks.errors.full_messages
+#     erb :"/potlucks/new"
+#   end
+# end
+
+get '/potlucks/:id' do
+  @potluck = find_and_ensure(params[:id])
+  erb :"/potlucks/show"
+end
+
+# get '/potlucks/:id/edit' do
+#   @potluck = find_and_ensure(params[:id])
+#   authenticate!
+#   authorize!(@potluck.owner)
+#   erb :"/potlucks/edit"
+# end
+
+# put '/potlucks/:id' do
+#   @potluck = find_and_ensure(params[:id])
+#   authenticate!
+#   authorize!(@potluck.owner)
+#   @potluck.assign_attributes(params[:potluck])
+
+#   if @potluck.save
+#     redirect "/potlucks/#{params[:id]}"
+#   else
+#     @errors = @potluck.errors.full_messages
+#     erb :'potlucks/edit'
+#   end
+# end
+
+# delete '/potlucks/:id' do
+#   @potluck = find_and_ensure(params[:id])
+#   authenticate!
+#   authorize!(@potluck.owner)
+#   @potluck.destroy
+#   redirect '/potlucks'
+# end
+
